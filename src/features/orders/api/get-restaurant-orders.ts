d7acb73953458575fa87ae7f8dd7b45e00f75e30
Andrew Ingombe <ingombe.ani@gmail.com>
2026-06-10 13:12:08 +0200

    chore:protect-admin-reads

--- src/features/orders/api/get-restaurant-orders.ts
+++ src/features/orders/api/get-restaurant-orders.ts
@@ -1,6 +1,15 @@
+import { getAdminToken } from "@/features/auth/utils/get-admin-token";
+
 export async function getRestaurantOrders(restaurantId: string) {
+  const token = getAdminToken();
+
   const res = await fetch(
     `http://localhost:3500/api/orders/restaurants/${restaurantId}`,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    },
   );
 
   if (!res.ok) {
